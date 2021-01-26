3329ba4680ff95984202121f787b9dd5209f77a5
suowenlong <1119649799@qq.com>
2021-01-26 14:44:05 +0800

    注册中心

---
+++ src/test/java/com/zdww/www/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdww.www.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
